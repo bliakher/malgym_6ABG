18dec97ba9d10a7ff3dcfa397b63cfd086a57dd6
Evgenia Golubeva <44325210+bliakher@users.noreply.github.com>
2021-01-18 12:46:56 +0100

    Update 16_hodina_reseni.py

--- zadani/16_hodina_reseni.py
+++ zadani/16_hodina_reseni.py
@@ -1,1 +1,33 @@
+# reading from file -----------------------------------
+
+f = open("a.txt", 'r')
+f.read()
+f.readline()
+
+# reading by line
+for line in f.readlies():
+  print(line)
+  
+# reading by character
+c = f.read(1)
+while c != "":
+  print(c)
+  c = f.read(1)
+  
+# writing to files -------------------------------------
+
+f = open(a.txt, 'w')
+f.write("The best text!") # this rewrites the whole file with new text
+
+f = open(a.txt, 'a')
+f.write("The best text!") # this adds new text at the end
+  
+# read from one file write to another ------------------
+
+a = open("a.txt", 'r')
+b = open("b.txt", 'w')
+content_a = f.read()
+b.write(content_a)
+a.close()
+b.close()
 
