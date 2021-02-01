d67a2c08661d82f8caa9c1cccd49454d00e01e25
Evgenia Golubeva <44325210+bliakher@users.noreply.github.com>
2021-02-01 12:40:35 +0100

    Add files via upload

---
+++ kod_z_hodin/kvinta_1_2.py
@@ -0,0 +1,21 @@
+'''
+abc x
+10 x
+2 3 5 x
+235 
+'''
+
+a = 2
+a = str(a) #--> a= "2"
+
+print(a+b+c)
+
+print(str(a) , str(b) , str(c))
+
+# pretizeny operator
+# overloaded, overconstraint operator
+
+# int + str -> error
+
+"hello"[1:]
+"hello"[:3]
