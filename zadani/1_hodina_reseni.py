67cca1007758afec2d3189e2ece94d80da2d0ef2
Evgenia Golubeva <44325210+bliakher@users.noreply.github.com>
2021-02-08 12:10:29 +0100

    Update 1_hodina_reseni.py

--- zadani/1_hodina_reseni.py
+++ zadani/1_hodina_reseni.py
@@ -22,6 +22,14 @@ with open("vstup.txt", "r") as f:
 # problem - ends of line
 # hello\nfriend - counts as on word
 
+"""
+hi   hello
+my friend
+
+-> split : ["hi", "hello\nmy", "friend"]
+"""
+
+
 # how to repair? -----------------------------
 # read text by lines:
 
@@ -37,4 +45,60 @@ print("Number of words", words_count)
 
 # whole file can't fit to RAM - we need to read it by parts
 # by lines or even by one character
+
+"""
+cyklus - cteme soubor po znacich, dokud ho nedocteme do konce
+znak    -> bily znak
+            -> docetli na konec slova -> pricteme +1 slovo
+        -> tisknutelny znak
+            -> cteme slovo
+
+"""
+
+def open_file(file_name):
+    f = open(file_name, 'r')
+    return f
+
+def count_words2(file):
+    count = 0
+    c = file.read(1)
+    word = ""
+    reading_word = False
+    while c != '':
+        if not is_space(c):     # is_space(c) == False
+            reading_word = True
+            word += c
+        elif reading_word:  # is_space(c) and reading_word
+            reading_word = False
+            count += 1
+            word = ""
+        c = file.read(1)
+    return count
+
+def count_words(file):
+    c = file.read(1)
+    word = ""
+    before = ''
+    count = 0
+    while c != '':
+        if is_space(c) and not is_space(before):
+            count += 1
+            word = ""
+        else:   # c je tisknutelny
+            word = word + c
+        before = c
+        c = file.read(1)
+    return count
+
+def is_space(ch):
+    return ch == ' ' or ch == '\n' or ch == '\r' or ch == '\t'
+
+def solve():
+    file_name = input("Enter name of the file:")
+    file = open_file(file_name)
+    words_count = count_words(file)
+    print("Number of words in file:", words_count)
+    file.close()
+
+solve()
 
