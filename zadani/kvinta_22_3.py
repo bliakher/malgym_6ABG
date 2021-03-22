8c3ce624b8cd488f8c0753a7afaf04e1fda57c2f
Evgenia Golubeva <44325210+bliakher@users.noreply.github.com>
2021-03-22 11:27:28 +0100

    Add files via upload

---
+++ zadani/kvinta_22_3.py
@@ -0,0 +1,20 @@
+
+# enter the application
+# menu: choose the sex of the baby
+# sex -> name_boy(), name_girl()
+
+
+# name_boy() -> name (string)
+
+# boys - name suggestions
+# suggest a name
+# ask if want another name
+
+# name_girl() -> name (string)
+
+# girls - name suggestions
+# suggest a name
+# ask if want another name
+
+# choose name -> return  to menu
+# name other child
