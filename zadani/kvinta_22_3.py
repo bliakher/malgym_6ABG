df334751101b5d3d4c74da68a1632f2042db70ef
Evgenia Golubeva <44325210+bliakher@users.noreply.github.com>
2021-03-22 11:51:31 +0100

    Update kvinta_22_3.py

--- zadani/kvinta_22_3.py
+++ zadani/kvinta_22_3.py
@@ -18,3 +18,35 @@
 
 # choose name -> return  to menu
 # name other child
+
+# boy naming part -----------------------------------------------------
+
+def get_boy_name():
+    boy_names = ["Maxík", "Kopáč", "Maxmilián', 'Klement", "Maxim"]
+    return rnd.choice(boy_names)
+
+
+def name_boy():
+    while True:
+        boy_name = get_boy_name()
+
+        print(f"Is {boy_name} ok?")
+        print("\t[A]:  Yes")
+        print("\t[B]:  No")
+        print("\t[ANY] Exit")
+        input_key = input()
+
+        if input_key == "a" or input_key == "A":
+            print("bááájo")
+            chosen_name = "Your name: " + boy_name
+            return chosen_name
+            break
+        elif input_key == "b" or input_key == "B":
+            continue
+        else:
+            break
+            return False
+
+
+result = name_boy()
+print(result)
