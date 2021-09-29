2aa1d835132fd993dc09a6951b6fc1d3a2a3e2ab
Evgenia Golubeva <44325210+bliakher@users.noreply.github.com>
2021-09-29 21:35:18 +0200

    Update sexta_24_9.py

--- kod_z_hodin/sexta_24_9.py
+++ kod_z_hodin/sexta_24_9.py
@@ -1,3 +1,28 @@
+def factorial(n):
+  f = 1
+  i = 1
+  while i <= n:
+    f = f * i
+    i += 1
+  print(f)
+
+def factorial_rec(n):
+  if n == 1:
+    return 1
+  # else 
+  f = n * factorial_rec(n-1)
+  return f
+
+factorial(5)
+print(factorial_rec(5))
+
+n = 5
+f = 1
+for i in range(1, n+1):
+  f = f * i
+
+print(f)
+
 vnoucata = []
 with open("rodokmen.txt", "r") as f:
   babicka = f.readline()
